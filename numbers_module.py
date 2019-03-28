7d030eb70605aa2f74b9414dc7fe1e554136147c
Kimberly Lawn <klawn1570@gmail.com>
2019-03-27 20:41:42 -0500

    first commit, determines is first number is larger

---
+++ numbers_module.py
@@ -0,0 +1,8 @@
+
+def main():
+    num_1 = int(input("Please enter a number: "))
+    num_2 = int(input("Please enter a number: "))
+
+    if num_1 > num_2:
+        print("num 1 is bigger than num 2")
+main()
